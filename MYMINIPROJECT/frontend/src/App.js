316668a521f87d699278fe0af47297166bbec7b0
Vivek <vivekkumar7458020087@gmail.com>
2023-09-17 11:06:28 +0530

    setup

--- MYMINIPROJECT/frontend/src/App.js
+++ MYMINIPROJECT/frontend/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TodoList from './components/TodoList';
+// import 'bootstrap/dist/css/bootstrap.css';
+import TodoInput from './components/TodoInput';
+import Navbar from './components/Navbar';
+import TenTodos from './components/TenTodos';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { store } from './reducers/store';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+
+		<BrowserRouter>
+			<Provider store={store}>
+				<div className="App">
+					<Navbar />
+					<TenTodos />
+					<Routes>
+						<Route exact path='/' component={TodoList} />
+						<Route path='/add' component={TodoInput} />
+						<Route path='/list' component={TodoList} />
+					</Routes>
+				</div>
+			</Provider>
+		</BrowserRouter>
+
+
+	);
 }
 
 export default App;
--- MYMINIPROJECT/frontend/src/components/BookSlot.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BookSlot = () => {
-  return (
-    <div>BookSlot</div>
-  )
-}
-
-export default BookSlot;
